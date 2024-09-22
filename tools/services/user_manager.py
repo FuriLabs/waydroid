07ffdbf4877a7ca94c65056a0cc6334968c57fbf
Bardia Moshiri <bardia@furilabs.com>
2024-09-21 21:09:15 -0400

    user_manager: also hide dialer and messaging
    Signed-off-by: Bardia Moshiri <bardia@furilabs.com>

--- tools/services/user_manager.py
+++ tools/services/user_manager.py
@@ -33,7 +33,8 @@ def start(args, session, unlocked_cb=None):
         # FuriOS: don't add an icon for default apps such as documents, settings or microG
         if packageName == "com.android.documentsui" or packageName == "com.android.inputmethod.latin" \
             or packageName == "com.android.settings" or packageName == "com.google.android.gms" \
-            or packageName == "org.lineageos.jelly" or packageName == "org.lineageos.aperture":
+            or packageName == "org.lineageos.jelly" or packageName == "org.lineageos.aperture" \
+            or packageName == "com.android.messaging" or packageName == "com.android.dialer":
             hide = True
 
         desktop_file_path = apps_dir + "/waydroid." + packageName + ".desktop"
