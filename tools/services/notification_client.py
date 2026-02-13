70d2089bfd48ab1a51f2a30d11c0e8eae6e3b00a
Bardia Moshiri <bardia@furilabs.com>
2026-02-13 17:40:20 -0500

    notification: rework with cmd notification
    Signed-off-by: Bardia Moshiri <bardia@furilabs.com>

--- tools/services/notification_client.py
+++ tools/services/notification_client.py
@@ -7,6 +7,7 @@ import dbus.service
 import dbus.mainloop.glib
 import threading
 from gi.repository import GLib
+
 from tools import helpers
 from tools import config
 from tools.helpers import ipc
@@ -18,11 +19,37 @@ stopping = False
 class NotificationService:
     def __init__(self, args):
         self.args = args
+
+        # Android-key -> desktop notification id
         self.open_notifications = {}
+        # desktop notification id -> Android-key
+        self.desktop_to_android_key = {}
+
+        # desktop notification id -> handler(action_key)
         self.action_handlers = {}
 
         dbus.mainloop.glib.DBusGMainLoop(set_as_default=True)
+
+        self._notifications_iface = None
+        self._android_iface = None
+
+        self.setup_dbus_proxies()
         self.setup_dbus_signals()
+
+    def setup_dbus_proxies(self):
+        bus = dbus.SessionBus()
+        notification_service = bus.get_object('org.freedesktop.Notifications',
+                                              '/org/freedesktop/Notifications')
+        self._notifications_iface = dbus.Interface(notification_service,
+                                                   dbus_interface='org.freedesktop.Notifications')
+
+        self._notifications_iface.connect_to_signal("ActionInvoked", self.on_action_invoked)
+
+        system_bus = dbus.SystemBus()
+        android_obj = system_bus.get_object('io.furios.Andromeda.Notification',
+                                            '/io/furios/Andromeda/Notification')
+        self._android_iface = dbus.Interface(android_obj,
+                                             dbus_interface='io.furios.Andromeda.Notification')
 
     def setup_dbus_signals(self):
         try:
@@ -80,13 +107,18 @@ class NotificationService:
 
         return False, None
 
-    ### Notification click actions ###
-
     def on_action_invoked(self, notification_id, action_key):
-        if notification_id in self.action_handlers:
-            handler = self.action_handlers[notification_id]
-            handler(action_key)
-            del self.action_handlers[notification_id]
+        try:
+            nid = int(notification_id)
+        except Exception:
+            return
+
+        if nid in self.action_handlers:
+            handler = self.action_handlers[nid]
+            try:
+                handler(action_key)
+            finally:
+                del self.action_handlers[nid]
 
     def create_action_handler(self, pkg_name):
         def handler(action_key):
@@ -102,30 +134,18 @@ class NotificationService:
                 app_manager.launch(args)
         return handler
 
-    ### Calls to freedesktop notification API ###
-
-    def notify_send(self, app_name, package_name, ticker, title, text, is_foreground_service,
-                    show_light, updates_id):
-        notification_id = 0
-
+    def notify_send(self, app_name, package_name, ticker, title, text,
+                    is_foreground_service, show_light, updates_id):
         # When the title and text fields are not present, we choose an empty title
         # and the ticker as text.
         if title == '' or text == '':
             title = ''
             text = ticker
 
-        bus = dbus.SessionBus()
-        notification_service = bus.get_object('org.freedesktop.Notifications',
-                                              '/org/freedesktop/Notifications')
-        notifications = dbus.Interface(notification_service,
-                                       dbus_interface='org.freedesktop.Notifications')
-        notifications.connect_to_signal("ActionInvoked", self.on_action_invoked)
-
-        notification_id = notifications.Notify(
+        nid = self._notifications_iface.Notify(
             app_name,
-            updates_id,
-            config.session_defaults["andromeda_data"] + "/icons/"
-            + package_name + ".png",
+            int(updates_id),
+            config.session_defaults["andromeda_data"] + "/icons/" + package_name + ".png",
             title,
             text,
             ['default', 'Open', 'open', 'Open'],
@@ -133,62 +153,130 @@ class NotificationService:
             5000
         )
 
-        self.action_handlers[int(notification_id)] = self.create_action_handler(package_name)
-        return notification_id
+        try:
+            return int(nid)
+        except Exception:
+            return 0
 
     def close_notification_send(self, notification_id):
-        bus = dbus.SessionBus()
-        notification_service = bus.get_object('org.freedesktop.Notifications',
-                                              '/org/freedesktop/Notifications')
-        notifications = dbus.Interface(notification_service,
-                                       dbus_interface='org.freedesktop.Notifications')
+        try:
+            self._notifications_iface.CloseNotification(int(notification_id))
+        except Exception as e:
+            logging.error(f"CloseNotification error: {e}")
 
-        notifications.CloseNotification(notification_id)
+        try:
+            if int(notification_id) in self.action_handlers:
+                del self.action_handlers[int(notification_id)]
+        except Exception:
+            pass
 
-        if notification_id in self.action_handlers:
-            del self.action_handlers[notification_id]
+    def on_new_message(self, msg_hash, _msg_id, package_name, ticker, title, text,
+                       is_foreground_service, is_group_summary, show_light, _when):
+        android_key = str(msg_hash)
 
-    ### Callbacks for subscribed notification server signals ###
+        logging.debug(
+            f"Received new message notification: "
+            f"{android_key}, {_msg_id}, {package_name}, {ticker}, {title}, {text}, "
+            f"{is_foreground_service}, {is_group_summary}, {show_light}, {_when}"
+        )
 
-    def on_new_message(self, msg_hash, _msg_id, package_name, ticker, title, text, is_foreground_service,
-                       is_group_summary, show_light, _when):
-        logging.debug(f"Received new message notification: {msg_hash}, {_msg_id}, {package_name}, " +
-                     f"{ticker}, {title}, {text}, {is_foreground_service}, {is_group_summary}, " +
-                     f"{show_light}, {_when}")
         try:
             ok, app_name = self.get_app_name(package_name)
             if ok and not is_group_summary:
-                notification_id = self.notify_send(app_name, package_name, ticker, title, text,
-                                                   is_foreground_service, show_light, 0)
-                self.open_notifications[msg_hash] = notification_id
+                nid = self.notify_send(app_name, package_name, ticker, title, text,
+                                       is_foreground_service, show_light, 0)
+                if nid != 0:
+                    self.open_notifications[android_key] = nid
+                    self.desktop_to_android_key[nid] = android_key
+                    self.action_handlers[nid] = self.create_action_handler(package_name)
         except dbus.DBusException:
             logging.error("Andromeda session is stopped")
+        except Exception as e:
+            logging.error(f"on_new_message error: {e}")
 
     def on_update_message(self, msg_hash, replaces_hash, _msg_id, package_name, ticker, title, text,
                           is_foreground_service, _is_group_summary, show_light, _when):
-        logging.debug(f"Received update message notification: {msg_hash}, {replaces_hash}, " +
-                     f"{_msg_id}, {package_name}, {ticker}, {title}, {text}, " +
-                     f"{is_foreground_service}, {_is_group_summary}, {show_light}, {_when}")
+        android_key = str(msg_hash)
+        replaces_key = str(replaces_hash)
+
+        logging.debug(
+            f"Received update message notification: "
+            f"{android_key}, {replaces_key}, {_msg_id}, {package_name}, {ticker}, "
+            f"{title}, {text}, {is_foreground_service}, {_is_group_summary}, "
+            f"{show_light}, {_when}"
+        )
+
         try:
             ok, app_name = self.get_app_name(package_name)
-            if ok and replaces_hash in self.open_notifications:
-                notification_id = self.notify_send(app_name, package_name, ticker, title, text,
-                                                   is_foreground_service, show_light,
-                                                   self.open_notifications[replaces_hash])
-                self.open_notifications[msg_hash] = notification_id
-                del self.open_notifications[replaces_hash]
-        except dbus.DBusException:
-            logging.error("Andromeda session is stopped")
+            if not ok:
+                return
 
-    # on android, a notification disappeared (and was not replaced by another)
-    def on_delete_message(self, msg_hash):
-        logging.debug(f"Received delete message notification: {msg_hash}")
-        try:
-            if msg_hash in self.open_notifications:
-                self.close_notification_send(self.open_notifications[msg_hash])
-                del self.open_notifications[msg_hash]
+            # If we already showed this notification, update it in-place.
+            if replaces_key in self.open_notifications:
+                existing_nid = self.open_notifications[replaces_key]
+                nid = self.notify_send(app_name, package_name, ticker, title, text,
+                                       is_foreground_service, show_light,
+                                       existing_nid)
+
+                if nid != 0 and nid != existing_nid:
+                    try:
+                        if existing_nid in self.desktop_to_android_key:
+                            del self.desktop_to_android_key[existing_nid]
+                    except Exception:
+                        pass
+                    self.desktop_to_android_key[nid] = android_key
+
+                    try:
+                        if existing_nid in self.action_handlers:
+                            del self.action_handlers[existing_nid]
+                    except Exception:
+                        pass
+                    self.action_handlers[nid] = self.create_action_handler(package_name)
+
+                self.open_notifications[android_key] = (nid if nid != 0 else existing_nid)
+
+                if android_key != replaces_key:
+                    try:
+                        del self.open_notifications[replaces_key]
+                    except Exception:
+                        pass
+            else:
+                if not _is_group_summary:
+                    nid = self.notify_send(app_name, package_name, ticker, title, text,
+                                           is_foreground_service, show_light, 0)
+                    if nid != 0:
+                        self.open_notifications[android_key] = nid
+                        self.desktop_to_android_key[nid] = android_key
+                        self.action_handlers[nid] = self.create_action_handler(package_name)
+
         except dbus.DBusException:
             logging.error("Andromeda session is stopped")
+        except Exception as e:
+            logging.error(f"on_update_message error: {e}")
+
+    def on_delete_message(self, msg_hash):
+        android_key = str(msg_hash)
+        logging.debug(f"Received delete message notification: {android_key}")
+
+        try:
+            if android_key in self.open_notifications:
+                nid = self.open_notifications[android_key]
+                self.close_notification_send(nid)
+
+                try:
+                    del self.open_notifications[android_key]
+                except Exception:
+                    pass
+
+                try:
+                    if nid in self.desktop_to_android_key:
+                        del self.desktop_to_android_key[nid]
+                except Exception:
+                    pass
+        except dbus.DBusException:
+            logging.error("Andromeda session is stopped")
+        except Exception as e:
+            logging.error(f"on_delete_message error: {e}")
 
     def run(self):
         self.args.notificationLoop = GLib.MainLoop()
